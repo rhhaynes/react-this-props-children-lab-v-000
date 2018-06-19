5daeffe0c881549c4142fc09bf2c1b494140494e
Randall Harris Haynes <harrishaynes@gmail.com>
2018-06-19 00:21:26 +0000

    automatically backed up by learn

--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,1 +1,11 @@
-// Code ThemedDecoration Component Here
+import Reach from 'react';
+
+export default class ThemedDecorations extends React.Component {
+  render() {
+    return (
+      <div className="themed-decorations">
+        <div className=""></h1>
+      </div>
+    );
+  }
+}
